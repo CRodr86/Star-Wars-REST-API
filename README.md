18d261e0689bcd005441c5c2b46e49493947bca9
CRodr86 <rodrigocfotografia@gmail.com>
2022-04-15 20:57:36 +0000

    finished exercise

--- README.md
+++ README.md
@@ -1,74 +1,29 @@
-# Flask Boilerplate for Profesional Development
-
-[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/from-referrer/)
 <p align="center">
-    <a href="https://youtu.be/ORxQ-K3BzQA"><img height="200px" src="https://github.com/4GeeksAcademy/flask-rest-hello/blob/main/docs/assets/how-to.png?raw=true?raw=true" /></a>
+  <img src="./docs/assets/4geeks.jpg">
 </p>
 
-## Features
+------------
 
-- Extensive documentation [here](https://github.com/4GeeksAcademy/flask-rest-hello/tree/master/docs).
-- Integrated with Pipenv for package managing.
-- Fast deloyment to heroku with `$ pipenv run deploy`.
-- Use of `.env` file.
-- SQLAlchemy integration for database abstraction.
+# Star Wars REST API
 
-## Installation (automatic if you are using gitpod)
+## Flask Exercise
 
-> Important: The boiplerplate is made for python 3.7 but you can easily change the `python_version` on the Pipfile.
+##### Made by Rodrigo Carvalho
 
-The following steps are automatically runned withing gitpod, if you are doing a local installation you have to do them manually:
+------------
 
-```sh
-pipenv install;
-mysql -u root -e "CREATE DATABASE example";
-pipenv run init;
-pipenv run migrate;
-pipenv run upgrade;
-```
+### Project:
+ 
+It's a REST API made with Flask for a Star Wars blog. 
 
-## How to Start coding?
+------------
 
-There is an example API working with an example database. All your application code should be written inside the `./src/` folder.
+### Instalation:
 
-- src/main.py (it's where your endpoints should be coded)
-- src/models.py (your database tables and serialization logic)
-- src/utils.py (some reusable classes and functions)
-- src/admin.py (add your models to the admin and manage your data easily)
+1. Get inside the environment `$ pipenv shell`
 
-For a more detailed explanation, look for the tutorial inside the `docs` folder.
+2. Run server `$ pipenv run start`
 
-## Remember to migrate every time you change your models
+------------
 
-You have to migrate and upgrade the migrations for every update you make to your models:
-```
-$ pipenv run migrate (to make the migrations)
-$ pipenv run upgrade  (to update your databse with the migrations)
-```
-
-
-# Manual Installation for Ubuntu & Mac
-
-⚠️ Make sure you have `python 3.6+` and `MySQL` installed on your computer and MySQL is running, then run the following commands:
-```sh
-$ pipenv install (to install pip packages)
-$ pipenv run migrate (to create the database)
-$ pipenv run start (to start the flask webserver)
-```
-
-
-## Deploy to Heroku
-
-This template is 100% compatible with Heroku[https://www.heroku.com/], just make sure to understand and execute the following steps:
-
-```sh
-// Install heroku
-$ npm i heroku -g
-// Login to heroku on the command line
-$ heroku login -i
-// Create an application (if you don't have it already)
-$ heroku create <your_application_name>
-// Commit and push to heroku (commited your changes)
-$ git push heroku main
-```
-:warning: For a more detailed explanation on working with .env variables or the MySQL database [read the full guide](https://github.com/4GeeksAcademy/flask-rest-hello/blob/master/docs/DEPLOY_YOUR_APP.md).
+Exercises made during the **Full Stack Developer Coding Bootcamp** at **4Geeks Academy**
--- src/main.py
+++ src/main.py
@@ -8,8 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Character, Planet, Favorites
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -30,14 +29,104 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+# User endpoints
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_users():
+    users = User.query.all()
+    all_users = list(map(lambda x: x.serialize(), users))
+    return jsonify(all_users), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_one_user(user_id):
+    user_x = User.query.get(user_id)
+    if user_x is None:
+        return 'User not found', 404
+    else:
+        return jsonify(user_x.serialize()), 200
 
-    return jsonify(response_body), 200
+@app.route('/user', methods=['POST'])
+def create_users():
+    request_body = request.get_json()
+    new_user = User(name= request_body["name"], email= request_body["email"], password= request_body["password"], favorite_planet= request_body["favorite_planet"], favorite_character= request_body["favorite_character"])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(request_body), 201
+
+@app.route('/user/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user_x = User.query.get(user_id)
+    if user_x is None:
+        return 'User not found', 404
+    else:
+        db.session.delete(user_x)
+        db.session.commit()
+        return jsonify("User deleted"), 200
+
+# Character endpoints
+@app.route('/character', methods=['GET'])
+def get_characters():
+    characters = Character.query.all()
+    all_characters = list(map(lambda x: x.serialize(), characters))
+    return jsonify(all_characters), 200
+
+@app.route('/character/<int:character_id>', methods=['GET'])
+def get_one_character(character_id):
+    character_x = Character.query.get(character_id)
+    if character_x is None:
+        return 'Character not found', 404
+    else:
+        return jsonify(character_x.serialize()), 200
+
+@app.route('/character', methods=['POST'])
+def create_character():
+    request_body = request.get_json()
+    new_character = Character(name= request_body["name"])
+    db.session.add(new_character)
+    db.session.commit()
+    return jsonify(request_body), 201
+
+@app.route('/character/<int:character_id>', methods=['DELETE'])
+def delete_character(character_id):
+    character_x = Character.query.get(character_id)
+    if character_x is None:
+        return 'Character not found', 404
+    else:
+        db.session.delete(character_x)
+        db.session.commit()
+        return jsonify("Character deleted"), 200
+
+#Planet endpoints
+@app.route('/planet', methods=['GET'])
+def get_planets():
+    planets = Planet.query.all()
+    all_planets = list(map(lambda x: x.serialize(), planets))
+    return jsonify(all_planets), 200
+
+@app.route('/planet/<int:planets_id>', methods=['GET'])
+def get_one_planet(planet_id):
+    planet_x = Planet.query.get(planet_id)
+    if planet_x is None:
+        return 'Planet not found', 404
+    else:
+        return jsonify(planet_x.serialize()), 200
+
+@app.route('/planet', methods=['POST'])
+def create_planet():
+    request_body = request.get_json()
+    new_planet = Planet(name= request_body["name"])
+    db.session.add(new_planet)
+    db.session.commit()
+    return jsonify(request_body), 201
+
+@app.route('/planet/<int:planet_id>', methods=['DELETE'])
+def delete_planet(planet_id):
+    planet_x = Planet.query.get(planet_id)
+    if planet_x is None:
+        return 'Planet not found', 404
+    else:
+        db.session.delete(planet_x)
+        db.session.commit()
+        return jsonify("Planet deleted"), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
